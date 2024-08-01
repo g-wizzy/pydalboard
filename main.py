9df71b1145f01278c88076b04db98d45fe305563
g-wizzy <pierre.burki@protonmail.com>
2024-08-01 18:50:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,26 +6,36 @@ import pyaudio
 from pydalboard.pipeline import Pipeline
 from pydalboard.signal import Wav
 from pydalboard.modules import Delay, DelayParameters
+from pydalboard.signal.base import SignalInfo
+from pydalboard.signal.oscillators import Oscillator, Waveform
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Argument needed: wav file")
         sys.exit()
 
-    source = Wav(Path(sys.argv[1]), loop=False)
-
     p = pyaudio.PyAudio()
     player = p.open(
-        rate=source.signal_info.sample_rate,
-        channels=(2 if source.signal_info.stereo else 1),
+        rate=44_800,
+        channels=2,
         output=True,
         frames_per_buffer=1,
         format=pyaudio.paInt32,
     )
 
-    pipeline = Pipeline(source)
+    pipeline = Pipeline(
+        # Wav(Path(sys.argv[1]), loop=False)
+        Oscillator(
+            waveform=Waveform.SINE,
+            frequency=440e3,
+            signal_info=SignalInfo(44_800, 32, True),
+        )
+    )
     pipeline.modules.append(Delay(DelayParameters(duration=10000, decay=0.2)))
 
     while True:
-        frame = pipeline.run()
-        player.write(frame.tobytes(), 1)
+        try:
+            frame = pipeline.run()
+            player.write(frame.tobytes(), 1)
+        except KeyboardInterrupt:
+            break
