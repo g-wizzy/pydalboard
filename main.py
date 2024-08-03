1bb513775010158e9e9ba0cb88f3fb69388b61d4
qtipee <quent.vallat@gmail.com>
2024-08-03 17:16:05 +0200

    Create Drive Module + Float32 Conversion

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 import sys
 
+import numpy as np
 import pyaudio
 
 from pydalboard.pipeline import Pipeline
 from pydalboard.signal import Wav
-from pydalboard.modules import Delay, DelayParameters, Filter, FilterParameters
+from pydalboard.modules import Delay, DelayParameters, Drive, DriveParameters, Filter, FilterParameters
 from pydalboard.signal.base import SignalInfo
 from pydalboard.signal.oscillators import Oscillator, Waveform
 
@@ -14,29 +15,47 @@ if __name__ == "__main__":
         print("Argument needed: wav file")
         sys.exit()
 
+    # Initialize PyAudio
     p = pyaudio.PyAudio()
+
+    wav_source = Wav(Path(sys.argv[1]), loop=False)
+    sample_rate = wav_source.signal_info.sample_rate
+    sample_format = wav_source.signal_info.sample_format
+    channels = 2 if wav_source.signal_info.stereo else 1
+
+    if sample_format == 16:
+        pa_format = pyaudio.paInt16
+    elif sample_format == 32:
+        pa_format = pyaudio.paInt32
+    else:
+        raise ValueError("Unsupported sample format")
+
     player = p.open(
-        rate=44_100,
-        channels=2,
+        rate=sample_rate,
+        channels=channels,
         output=True,
         frames_per_buffer=1,
-        format=pyaudio.paInt32,
+        format=pa_format,
     )
 
     pipeline = Pipeline(
-        Wav(Path(sys.argv[1]), loop=False)
+        wav_source
         # Oscillator(
         #     waveform=Waveform.SINE,
         #     frequency=440e3,
         #     signal_info=SignalInfo(44_800, 32, True),
         # )
     )
-    pipeline.modules.append(Delay(DelayParameters(duration=10000, decay=0.5)))
-    pipeline.modules.append(Filter(FilterParameters(cutoff=2000, resonance=1.414, filter_type='band', slope=12)))
+    pipeline.modules.append(Drive(DriveParameters(gain=3.0, clipping=True)))
+    # pipeline.modules.append(Filter(FilterParameters(cutoff=3000, resonance=1.414, filter_type='low', slope=12)))
+    # pipeline.modules.append(Delay(DelayParameters(duration=10000, decay=0.5)))
     
     while True:
         try:
             frame = pipeline.run()
+            if wav_source.signal_info.sample_format in [16, 32]:
+                # Convert back from float32 to int16/32
+                frame = (frame * (2**31 - 1)).astype(np.int32)
             player.write(frame.tobytes(), 1)
         except KeyboardInterrupt:
             break
---
+++ pydalboard/modules/drive.py
@@ -0,0 +1,28 @@
+from typing import ClassVar
+from dataclasses import dataclass
+
+import numpy as np
+
+from pydalboard.signal import SignalInfo
+from pydalboard.modules.base import Module
+
+@dataclass
+class DriveParameters:
+    gain: float # Amount of drive to add to the signal
+    clipping: bool # Whether to clip the signal or not
+
+    def __post_init__(self):
+        self.gain = max(1.0, self.gain)
+
+class Drive(Module):
+    def __init__(self, params: DriveParameters):
+        self.params = params
+
+    def process(self, input: np.ndarray, signal_info: SignalInfo) -> np.ndarray:
+        output = input * self.params.gain
+
+        # Clip the output to avoid distortion
+        if self.params.clipping:
+            output = np.clip(output, -1.0, 1.0)
+
+        return output
